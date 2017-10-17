7eb7b0c4b28fddbc74eb0a691ec988531f21b098
s.tkachenko <s.tkachenko@corp.mail.ru>
2017-10-17 13:10:35 +0300

    add new query limits

--- src/api/queries.js
+++ src/api/queries.js
@@ -9,6 +9,7 @@ export const modificationsWithMatchedProductQuery = (prop, matcher) => {
     productQuery.matches(prop, matcher);
 
     return modificationQuery
+        .limit(1000)
         .include(['product.category'])
         .matchesQuery('product', productQuery);
 };
@@ -17,5 +18,6 @@ export const modificationsQuery = () => {
     const query = new Parse.Query(Modification);
 
     return query
+        .limit(2000)
         .include(['product.category']);
 };
